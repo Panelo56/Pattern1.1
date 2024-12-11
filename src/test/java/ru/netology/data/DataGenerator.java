4f83b56f7609967ed132d8051ad1d5dc5c2081dd
Panelo56 <metainf12@mail.ru>
2024-12-11 19:45:22 +0300

    Initiall commit

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,52 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    private static final Faker faker = new Faker(new Locale("ru"));
+
+    public static String generateDate(int shift) {
+        String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    public static String generateCity(String locale) {
+        String city = faker.address().city();
+        return city;
+    }
+
+    public static String generateName(String locale) {
+        String name = faker.name().fullName();
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
+
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
